6c03c2ec666c0aeead20e211e5a7d1193e3fedc9
Noam <noamg@cox.net>
2016-03-28 20:58:16 -0400

    utube

--- public/app.js
+++ public/app.js
@@ -1,14 +1,36 @@
 var app = angular.module('Tube', []);
 
-app.controller('CtrlTube', function($scope) {
-  $scope.code = '2O7wxkw1ras';
+app.run(function () {
+  var tag = document.createElement('script');
+  tag.src = "http://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
-app.directive('myYoutube', function($sce) {
+
+app.config( function ($httpProvider) {
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+app.controller('myYoutube', function($scope) {
+//  $scope.code = 'BS0T8Cd4UhA';
+$scope.search = function () {
+   $http.get('https://www.googleapis.com/youtube/v3/search', {
+     params: {
+       key: 'AIzaSyD2K6OooNWMPgEWlkAkgAIRctksFyKk1vY',
+       type: 'video',
+       maxResults: '8',
+       part: 'id,snippet',
+       fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+       q: this.query
+     }
+   })
+});
+app.directive('controller', function($sce) {
   return {
     restrict: 'EA',
     scope: { code:'=' },
     replace: true,
-    template: '<div style="height:350px;"><iframe style="overflow:hidden;height:350px;width:400px" width="400px" height="350px" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
+    template: '<div style="height:350px;"><iframe style="overflow:hidden;height:350px;width:450px" width="450px" height="350px" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
     link: function (scope) {
         console.log('here');
         scope.$watch('code', function (newVal) {
