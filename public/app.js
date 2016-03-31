1d8e0ee2e75adb7a9a5327bac30751dfa7b639b9
Noam <noamg@cox.net>
2016-03-30 21:00:31 -0400

    utube

--- public/app.js
+++ public/app.js
@@ -7,6 +7,25 @@ app.run(function () {
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
 
+  // app.controller('CtrlTube', function ($scope, Videos) {
+  //
+  //    init();
+  //
+  //    function init() {
+  //   $scope.youtube = VideosService.getYoutube();
+  //   $scope.results = VideosService.getResults();
+  //   $scope.upcoming = VideosService.getUpcoming();
+  //   $scope.history = VideosService.getHistory();
+  //   $scope.playlist = true;
+  // }
+  //
+  // $scope.launch = function (id, title) {
+  //   VideosService.launchPlayer(id, title);
+  //   VideosService.archiveVideo(id, title);
+  //   VideosService.deleteVideo($scope.upcoming, id);
+  //   $log.info('Launched id:' + id + ' and title:' + title);
+  // };
+
 app.controller('CtrlTube', function($scope) {
 $scope.search = function () {
    $http.get('https://www.googleapis.com/youtube/v3/search', {
@@ -29,31 +48,15 @@ app.config( function ($httpProvider) {
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
 
-// app.controller('VideosController', function ($scope, $http, $log, VideosService) {
-//
-//     init();
-//
-//     function init() {
-//       $scope.youtube = VideosService.getYoutube();
-//       $scope.results = VideosService.getResults();
-//       $scope.playlist = true;
-//     }
-//
-//     $scope.launch = function (id, title) {
-//       VideosService.launchPlayer(id, title);
-//       VideosService.archiveVideo(id, title);
-//       $log.info('Launched id:' + id + ' and title:' + title);
-//     };
-
-
 app.directive('myYoutube', function($sce) {
+  $scope.search.post('<iframe style="overflow:hidden;height:350px;width:450px" width="450px" height="350px" src="{{code}}" frameborder="0" allowfullscreen></iframe>')
   return {
     restrict: 'EA',
     scope: { code:'=' },
     replace: true,
     template: '<div style="height:350px;"><iframe style="overflow:hidden;height:350px;width:450px" width="450px" height="350px" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
     link: function (scope) {
-        console.log('here');
+        console.log('code');
         scope.$watch('code', function (newVal) {
            if (newVal) {
                scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + newVal);
